636783f2aebae54a0aecde5803c9e0d5769e2928
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-03-01 16:23:57 -0300

    feat: login and display data user on app

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,84 @@
-import React from 'react';
-import { SafeAreaView, StatusBar, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar } from 'react-native';
+import {
+  LoginButton,
+  LoginResult,
+  AccessToken,
+  GraphRequest,
+  GraphRequestManager,
+} from 'react-native-fbsdk';
 
-import Test from '@src/components/Test';
+import { Container, UserData, Loading, Text } from './styles';
 
-const App: React.FC = () => (
-  <>
-    <StatusBar barStyle="dark-content" />
-    <SafeAreaView>
-      <Test />
-      <Text>Hello world</Text>
-    </SafeAreaView>
-  </>
-);
+interface IUser {
+  name: string;
+  email: string;
+}
+
+const App: React.FC = () => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  function getUserCallback(error?: object, result?: object) {
+    if (error) {
+      console.log('getUserError', error);
+    } else if (result) {
+      setUser(result);
+      setLoading(false);
+    }
+  }
+
+  function getUserInfo(token: string) {
+    const infoRequest = new GraphRequest(
+      '/me',
+      {
+        accessToken: token,
+        parameters: {
+          fields: { string: 'email, name' },
+        },
+      },
+      getUserCallback,
+    );
+
+    new GraphRequestManager().addRequest(infoRequest).start();
+  }
+
+  async function finishLogin(error: object, result: LoginResult) {
+    if (error) {
+      console.log(error);
+    } else if (result.isCancelled) {
+      console.log('isCancelled');
+    } else {
+      const accessData = await AccessToken.getCurrentAccessToken();
+
+      if (accessData?.accessToken) {
+        setLoading(true);
+
+        getUserInfo(accessData?.accessToken);
+      }
+    }
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Container>
+        <UserData>
+          {loading && <Loading />}
+          {user && (
+            <>
+              <Text>{user.name}</Text>
+              <Text>{user.email}</Text>
+            </>
+          )}
+        </UserData>
+        <LoginButton
+          permissions={['public_profile', 'email']}
+          onLoginFinished={finishLogin}
+        />
+      </Container>
+    </>
+  );
+};
 
 export default App;
---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+export const UserData = styled.View``;
+export const Loading = styled.ActivityIndicator``;
+export const Text = styled.Text`
+  font-size: 14px;
+  color: #333;
+  margin-bottom: 4px;
+`;
